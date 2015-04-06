644f10073c53a8ba973b1e3ab89727f408c19320
eldraco <eldraco@gmail.com>
2015-04-06 13:48:49 +0200

    better

--- TheInfiniteTwistedWebsite.py
+++ TheInfiniteTwistedWebsite.py
@@ -4,6 +4,10 @@ from twisted.web.server import Site # An IProtocolFactory wich glues a listening
 from twisted.internet import reactor # drives the whole process, accepting TCP connections and moving bytes
 from twisted.web.resource import Resource
 from twisted.web.server import NOT_DONE_YET
+from twisted.internet import defer
+from twisted.web import http
+from twisted.internet import protocol
+
 import datetime
 
 
@@ -43,8 +47,57 @@ class Infinite(Resource):
     
 
 
-resource = Infinite()
-factory = Site(resource) # Bind them to the site
-reactor.listenTCP(8000, factory) # Listen in this port
+#resource = Infinite()
+#factory = Site(resource) # Bind them to the site
+#reactor.listenTCP(8000, factory) # Listen in this port
+#reactor.run()
+
+clients = {}
+
+class cli():
+    def __init__(self):
+        self.connectionTime = -1
+        self.disconnectionTime = -1
+        self.amountTransfered = 0
+
+#####################
+def wait(seconds, result=None):
+    """Returns a deferred that will be fired later"""
+    d = defer.Deferred()
+    reactor.callLater(seconds, d.callback, result)
+    return d
+
+class StreamHandler(http.Request):
+
+    @defer.inlineCallbacks
+    def process(self):
+        newcli = cli()
+        newcli.connectionTime = datetime.datetime.now()
+        clients[http.Request.getClientIP(self)] = newcli
+        #print dir(http.Request)
+        useragent = http.Request.getAllHeaders(self)['user-agent']
+        print 'Client connected: {} on {}. User-Agent: {}'.format(http.Request.getClientIP(self), clients[http.Request.getClientIP(self)].connectionTime, useragent)
+        while True:
+            self.setHeader('Connection', 'Keep-Alive')
+            self.setHeader('Content-Type', "multipart/x-mixed-replace")
+            self.write("Content-Type: text/html\n")
+            s = "A"*1024
+            newcli.amountTransfered += len(s)
+            self.write(s)
+            yield wait(0)
+
+    def connectionLost(self,reason):
+        disconnect_time = datetime.datetime.now()
+        print 'Client disconnected: {}. Duration: {}. Transfered: {:.2f} MB'.format(http.Request.getClientIP(self), disconnect_time - clients[http.Request.getClientIP(self)].connectionTime, clients[http.Request.getClientIP(self)].amountTransfered/1024/1024.0)
+
+
+class StreamProtocol(http.HTTPChannel):
+    requestFactory = StreamHandler
+
+class StreamFactory(http.HTTPFactory):
+    protocol = StreamProtocol
+
+reactor.listenTCP(8800, StreamFactory())
 reactor.run()
+
 
