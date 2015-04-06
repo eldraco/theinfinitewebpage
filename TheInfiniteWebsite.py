80cc22ed4bf8df7ecd859f17a0c0c8b65396514e
eldraco <eldraco@gmail.com>
2015-04-06 14:08:15 +0200

    working but old

--- TheInfiniteWebsite.py
+++ TheInfiniteWebsite.py
@@ -17,10 +17,11 @@ Usage: Just execute it and then download the web page with wget or something.
 '''
 
 import BaseHTTPServer
-import gzip
 import SocketServer
+import sys
 import time
 import curses
+import socket
 #from signal import signal, SIGPIPE, SIG_DFL, SIG_IGN
 #signal(SIGPIPE,SIG_DFL) 
 
@@ -45,8 +46,7 @@ new_screen.addstr(0,0, 'Live Log')
 screen = new_screen
 
 
-class ChunkingHTTPServer(SocketServer.ThreadingMixIn,
-                        BaseHTTPServer.HTTPServer):
+class ChunkingHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     '''
     This is just a proof of concept server that uses threads. You can make it
     fork, maybe hack up a worker thread model, or even use multiprocessing.
@@ -54,36 +54,6 @@ class ChunkingHTTPServer(SocketServer.ThreadingMixIn,
     chunked data from a server.
     '''
     daemon_threads = True
-
-class ListBuffer(object):
-    '''
-    This little bit of code is meant to act as a buffer between the optional
-    gzip writer and the actual outgoing socket - letting us properly construct
-    the chunked output. It also lets us quickly and easily determine whether
-    we need to flush gzip in the case where a user has specified
-    'ALWAYS_SEND_SOME'.
-    
-    This offers a minimal interface necessary to back a writing gzip stream.
-    '''
-
-    __slots__ = 'buffer',
-    def __init__(self):
-        self.buffer = []
-
-    def __nonzero__(self):
-        return len(self.buffer)
-
-    def write(self, data):
-        if data:
-            self.buffer.append(data)
-
-    def flush(self):
-        pass
-
-    def getvalue(self):
-        data = ''.join(self.buffer)
-        self.buffer = []
-        return data
 
 class ChunkingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     global y_pos
@@ -98,7 +68,25 @@ class ChunkingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     ALWAYS_SEND_SOME = False
     ALLOW_GZIP = False
     protocol_version = 'HTTP/1.1'
-    #(y_max, x_max) = self.screen.getmaxyx()
+
+    def finish(self):
+        try:
+            if not self.wfile.closed:
+                self.wfile.close()
+                self.wfile.flush()
+            self.wfile.close()
+            self.rfile.close()
+        except socket.error, e:
+            print "socket error"
+        except Exception as inst:
+            print 'Problem in finish()'
+            print type(inst)     # the exception instance
+            print inst.args      # arguments stored in .args
+            print inst           # __str__ allows args to printed directly
+
+
+
+
 
     def do_GET(self):
         global y_pos
@@ -121,25 +109,30 @@ class ChunkingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         def write_chunk():
             tosend = '%X\r\n%s\r\n'%(len(chunk), chunk)
-            self.wfile.write(tosend)
+            try:
+                self.wfile.write(tosend)
+            except socket.error, e:
+                self.request.close()
+                pass
+                raise
 
         # get some chunks
         for chunk in chunk_generator(self.client_address):
             if not chunk:
                 continue
-
-            write_chunk()
+            try:
+                write_chunk()
+            except socket.error, e:
+                break
 
         # no more chunks!
 
         # send the chunked trailer
-        self.wfile.write('0\r\n\r\n')
+        #self.wfile.write('0\r\n\r\n')
 
 def chunk_generator(client):
     global clients
     import datetime
-    # generate some chunks
-    #for i in xrange(10):
     amount = 0
     while True:
         i = "<html>"
@@ -155,6 +148,11 @@ def chunk_generator(client):
             screen.refresh()
 
 if __name__ == '__main__':
-    server = ChunkingHTTPServer( ('0.0.0.0', 8080), ChunkingRequestHandler)
-    print 'Starting server, use <Ctrl-C> to stop'
-    server.serve_forever()
+    try:
+        server = ChunkingHTTPServer( ('0.0.0.0', 8080), ChunkingRequestHandler)
+        print 'Starting server, use <Ctrl-C> to stop'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        curses.echo()
+        curses.nocbreak()
+        sys.exit(-1)
