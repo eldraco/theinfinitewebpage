c7141a8a591e40dcc437c259ef0163acc9ba97d5
eldraco <eldraco@gmail.com>
2015-04-06 14:44:54 +0200

    solve the finish issue

--- TheInfiniteWebsite.py
+++ TheInfiniteWebsite.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python
-'''
-This code is almost exactly the same as the chunked_server_test.py from josiahcarlson (https://gist.github.com/josiahcarlson/3250376). I only putted the infinite part.
+from twisted.internet import reactor # drives the whole process, accepting TCP connections and moving bytes
+from twisted.internet import defer
+from twisted.web import http
+from twisted.internet import protocol
 
-Copyright March 25, 2015
-Released into the public domain
-
-This implements an Infinite chunked server using Python threads and the built-in
-BaseHTTPServer module. Enable gzip compression at your own peril - web
-browsers seem to have issues, though wget, curl, Python's urllib2, my own
-async_http library, and other command-line tools have no problems.
-
-The infinite idea is of Sebastian Garcia: eldraco@gmail.com
-
-Usage: Just execute it and then download the web page with wget or something.
-
-'''
-
-import BaseHTTPServer
-import SocketServer
-import sys
-import time
+import datetime
 import curses
-import socket
-#from signal import signal, SIGPIPE, SIG_DFL, SIG_IGN
-#signal(SIGPIPE,SIG_DFL) 
+
+clients = {}
+
+class cli():
+    def __init__(self):
+        self.connectionTime = -1
+        self.disconnectionTime = -1
+        self.amountTransfered = 0
+        self.y_pos = -1
 
 # Global pos
 y_pos = 0
 clients = {}
-# Initialize the curses
+
+# Initialize the curses       
 stdscr = curses.initscr()
 curses.start_color()
 curses.use_default_colors()
@@ -46,113 +38,67 @@ new_screen.addstr(0,0, 'Live Log')
 screen = new_screen
 
 
-class ChunkingHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
-    '''
-    This is just a proof of concept server that uses threads. You can make it
-    fork, maybe hack up a worker thread model, or even use multiprocessing.
-    That's your business. But as-is, it works reasonably well for streaming
-    chunked data from a server.
-    '''
-    daemon_threads = True
+#####################
+def wait(seconds, result=None):
+    """Returns a deferred that will be fired later"""
+    d = defer.Deferred()
+    reactor.callLater(seconds, d.callback, result)
+    return d
 
-class ChunkingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class StreamHandler(http.Request):
     global y_pos
-    '''
-    Nothing is terribly magical about this code, the only thing that you need
-    to really do is tell the client that you're going to be using a chunked
-    transfer encoding.
-
-    Gzip compression works partially. See the module notes for more
-    information.
-    '''
-    ALWAYS_SEND_SOME = False
-    ALLOW_GZIP = False
-    protocol_version = 'HTTP/1.1'
-
-    def finish(self):
-        try:
-            if not self.wfile.closed:
-                self.wfile.close()
-                self.wfile.flush()
-            self.wfile.close()
-            self.rfile.close()
-        except socket.error, e:
-            print "socket error"
-        except Exception as inst:
-            print 'Problem in finish()'
-            print type(inst)     # the exception instance
-            print inst.args      # arguments stored in .args
-            print inst           # __str__ allows args to printed directly
-
-
-
-
-
-    def do_GET(self):
-        global y_pos
-        # Store the new client with its address
-        clients[self.client_address] = y_pos
-
-        screen.addstr(clients[self.client_address],0,self.path+": "+self.client_address[0])
-        y_pos += 1
-        screen.refresh()
-
-        #ae = self.headers.get('accept-encoding') or ''
-
-        # send some headers
-        self.send_response(200)
-        self.send_header('Transfer-Encoding', 'chunked')
-        self.send_header('Content-type', 'text/plain')
-
-
-        self.end_headers()
-
-        def write_chunk():
-            tosend = '%X\r\n%s\r\n'%(len(chunk), chunk)
-            try:
-                self.wfile.write(tosend)
-            except socket.error, e:
-                self.request.close()
-                pass
-                raise
-
-        # get some chunks
-        for chunk in chunk_generator(self.client_address):
-            if not chunk:
-                continue
-            try:
-                write_chunk()
-            except socket.error, e:
-                break
-
-        # no more chunks!
-
-        # send the chunked trailer
-        #self.wfile.write('0\r\n\r\n')
-
-def chunk_generator(client):
     global clients
-    import datetime
-    amount = 0
-    while True:
-        i = "<html>"
-        i += "TheInfiniteWebsite" * 20000
-        amount += len(i)
-        time.sleep(.1)
-        try:
-            yield "%s\r\n"%i
-            screen.addstr(clients[client],80,"Amount sent: "+str(amount/1024/1024)+" MB")
-            screen.refresh()
-        except:
-            screen.addstr(clients[client],110,"Ended: "+str(datetime.datetime.now()))
-            screen.refresh()
 
-if __name__ == '__main__':
-    try:
-        server = ChunkingHTTPServer( ('0.0.0.0', 8080), ChunkingRequestHandler)
-        print 'Starting server, use <Ctrl-C> to stop'
-        server.serve_forever()
-    except KeyboardInterrupt:
-        curses.echo()
-        curses.nocbreak()
-        sys.exit(-1)
+    @defer.inlineCallbacks
+    def process(self):
+        global y_pos
+        global clients
+        newcli = cli()
+        newcli.connectionTime = datetime.datetime.now()
+        clients[http.Request.getClientIP(self)] = newcli
+        clients[http.Request.getClientIP(self)].y_pos = y_pos
+        y_pos += 1
+
+        useragent = http.Request.getAllHeaders(self)['user-agent']
+        # Print
+        screen.addstr(clients[http.Request.getClientIP(self)].y_pos,0, "Client "+http.Request.getClientIP(self)+' connected at '+str(clients[http.Request.getClientIP(self)].connectionTime)+'. User-Agent: '+useragent)
+        screen.refresh()
+        # http.HTTPChannel
+        # ['MAX_LENGTH', '_HTTPChannel__content', '_HTTPChannel__first_line', '_HTTPChannel__header', '_TimeoutMixin__timedOut', '_TimeoutMixin__timeoutCall', '__doc__', '__implemented__', '__init__', '__module__', '__providedBy__', '__provides__', '_buffer', '_busyReceiving', '_finishRequestBody', '_receivedHeaderCount', '_savedTimeOut', 'allContentReceived', 'allHeadersReceived', 'callLater', 'checkPersistence', 'clearLineBuffer', 'connected', 'connectionLost', 'connectionMade', 'dataReceived', 'delimiter', 'headerReceived', 'length', 'lineLengthExceeded', 'lineReceived', 'line_mode', 'logPrefix', 'makeConnection', 'maxHeaders', 'pauseProducing', 'paused', 'persistent', 'rawDataReceived', 'requestDone', 'requestFactory', 'resetTimeout', 'resumeProducing', 'sendLine', 'setLineMode', 'setRawMode', 'setTimeout', 'stopProducing', 'timeOut', 'timeoutConnection', 'transport']
+
+
+
+        # http.Request
+        # ['__doc__', '__implemented__', '__init__', '__module__', '__providedBy__', '__provides__', '__repr__', '__setattr__', '_authorize', '_cleanup', '_disconnected', '_forceSSL', '_warnHeaders', 'addCookie', 'args', 'chunked', 'clientproto', 'code', 'code_message', 'connectionLost', 'content', 'etag', 'finish', 'finished', 'getAllHeaders', 'getClient', 'getClientIP', 'getCookie', 'getHeader', 'getHost', 'getPassword', 'getRequestHostname', 'getUser', 'gotLength', 'handleContentChunk', 'headers', 'isSecure', 'lastModified', 'method', 'noLongerQueued', 'notifyFinish', 'parseCookies', 'path', 'process', 'producer', 'received_headers', 'redirect', 'registerProducer', 'requestReceived', 'sentLength', 'setETag', 'setHeader', 'setHost', 'setLastModified', 'setResponseCode', 'startedWriting', 'unregisterProducer', 'uri', 'write']
+
+
+        while not http.Request.finished:
+                self.setHeader('Connection', 'Keep-Alive')
+                s = "A"*1024
+                newcli.amountTransfered += len(s)
+                # For some reason the connection is not stopped and continues to try to send data
+                #screen.addstr(clients[http.Request.getClientIP(self)].y_pos,100, "Transfered "+str(clients[http.Request.getClientIP(self)].amountTransfered/1024/1024.0)+' MB')
+                #screen.refresh()
+                try:
+                    self.write(s)
+                    yield wait(0)
+                except:
+                    return
+
+    def connectionLost(self,reason):
+        global clients
+        disconnect_time = datetime.datetime.now()
+        screen.addstr(clients[http.Request.getClientIP(self)].y_pos,120, "Duration "+str(disconnect_time - clients[http.Request.getClientIP(self)].connectionTime)+'. Transfered: '+str(clients[http.Request.getClientIP(self)].amountTransfered/1024/1024.0)+' MB')
+        screen.refresh()
+        http.Request.notifyFinish(self)
+        http.Request.finish(self)
+
+
+class StreamProtocol(http.HTTPChannel):
+    requestFactory = StreamHandler
+
+class StreamFactory(http.HTTPFactory):
+    protocol = StreamProtocol
+
+reactor.listenTCP(8800, StreamFactory())
+reactor.run()
